ca3286debdd35b399a05b281a19474a9ed3a22c0
Eunhae Seo <clemenyy@gmail.com>
2017-07-30 16:04:14 +0900

    jhgjhgjh

---
+++ js/app.js
@@ -0,0 +1,34 @@
+// var weapons = ["AK47", "Magnum", "Kim jong fat"];
+// var soldier = {
+//   name:"Nicolas",
+//   age: 22,
+//   weapon: weapons[2],
+//   isKorean: false,
+//   isDead: false,
+//   sayHi: function() {
+//     console.log("Hello, my name is" + this.name + "and I'm:" + this.age + a);
+//   }
+// };
+//
+//
+// function multiply(A, B) {
+//   return A * B;
+// }
+//
+// function sum(A, B) {
+//   return A + B;
+// }
+//
+// function divide(A, B) {
+//   return A / B;
+// }
+//
+// function expo(A, B) {
+//   // C=1;
+//   // for (i=0; i<B; i++)
+//   // {
+//   //   C=C*A;
+//   // }
+//   // return C;
+//   A**B;
+// }
